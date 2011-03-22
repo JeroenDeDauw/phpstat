475fb751faca7c6af5b6ddc4079c15dd42f95e4e
Jeroen De Dauw <jeroendedauw@gmail.com>
2011-03-22 17:35:30 +0100

    added function to walk through a dir recursivly

--- src/phpstat/DirWalker.py
+++ src/phpstat/DirWalker.py
@@ -4,19 +4,45 @@ Created on Mar 21, 2011
 @author: jeroen
 '''
 
+import os
+
 class DirWalker(object):
     '''
     classdocs
     '''
-
-
+    
+    _files = []
+    
     def __init__(self):
         '''
         Constructor
         '''
         
         
+    def walk(self, rootdir):
+        fileList = {}
+        dirStack = [rootdir]
+
+        while dirStack:
+            directory = dirStack.pop()
+            fileList[directory] = []
+            
+            for item in os.listdir(directory):
+                if not item.startswith('.'):
+                    fullname = os.path.join(directory, item)
+                    
+                    if os.path.isdir(fullname):
+                        if not os.path.islink(fullname): dirStack.append(fullname)
+                    else :
+                        fileList[directory].append(item)
         
-    def walk(self):
+        self._files =  fileList 
         
-        return 'ohai'
+    def get_file_stats(self, filepath):
+        return filepath
+    
+    def get_stats(self):
+        files = []
+        for dir, data in self._files.iteritems():
+            files.append(dir + "\n-- " + "\n-- ".join(data) + "\n")
+        return '\n'.join(files)
--- src/phpstat/__init__.py
+++ src/phpstat/__init__.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def create_list():
     walker = DirWalker()
-    
-    return walker.walk()
+    walker.walk( '/var/www/extensions/Maps/' )
+    return walker.get_stats()
 
 if __name__ == '__main__':
     app.run(debug=True)
